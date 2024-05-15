2e45ba594e43455c88dc4267ea154556ae4d40f0
ethanillingsworth <illingsworth.ethan@icloud.com>
2024-05-15 09:00:42 -0500

    Create Colors.swift

---
+++ src/Colors.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+class Colors {
+    static let textColor = Color.black
+    static let accent = Color(red: 0.455, green: 0.929, blue: 0.929) // #74eded
+    static let lightGray = Color(red: 0.953, green: 0.953, blue: 0.953) // #f3f3f3
+    static let gray = Color(red: 0.906, green: 0.906, blue: 0.906) // #e7e7e7
+    static let gray2 = Color(red: 0.757, green: 0.757, blue: 0.757) // #c1c1c1
+    static let gray3 = Color(red: 0.51, green: 0.51, blue: 0.51) // #828282
+    static let gray4 = Color(red: 0.427, green: 0.427, blue: 0.427) // #6d6d6d
+    static let background = Color(red: 0.882, green: 0.882, blue: 0.882) // #e1e1e1
+    
+    
+    static let textColorDark = Color.white
+    static let accentDark = Color.pink
+    static let lightGrayDark = Color.pink
+    static let grayDark = Color(red: 0.3, green: 0.3, blue: 0.3) // #868686
+    static let gray2Dark = Color(red: 0.35, green: 0.35, blue: 0.35) // #606060
+    static let gray3Dark = Color(red: 0.4, green: 0.4, blue: 0.4) // #828282
+    static let gray4Dark = Color(red: 0.427, green: 0.427, blue: 0.427) // #6d6d6d
+    static let backgroundDark = Color(red: 0.255, green: 0.255, blue: 0.255) // #414141
+    
+}
